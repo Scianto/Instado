7226a026895ef03f8b684440ca9d7229c4b9a4a9
sci anto <65482441+Scianto@users.noreply.github.com>
2020-07-06 22:01:19 +0530

    Add files via upload

---
+++ Instado.py
@@ -0,0 +1,12 @@
+import webbrowser
+print (f"""
+      {\033[1;36m}
+    
+ ▀ █▄░█ ▄▀▀ ▀█▀ ▄▀▄ █▀▄ ▄▀▄
+ █ █░▀█ ░▀▄ ░█░ █▀█ █░█ █░█
+ ▀ ▀░░▀ ▀▀░ ░▀░ ▀░▀ ▀▀░ ░▀░
+         .:created by sci_anto:.
+       """)
+post_url = input("Enter post url:") 
+download_url = "savefrom.net/"+post_url
+webbrowser.open(download_url)
