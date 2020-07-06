277bc358fc984491fc62cf90505bbd68052a5411
sci anto <65482441+Scianto@users.noreply.github.com>
2020-07-06 22:04:23 +0530

    Update Instado.py

--- Instado.py
+++ Instado.py
@@ -1,12 +1,12 @@
 import webbrowser
-print (f"""
-      {\033[1;36m}
-    
+print (033[1;36m
+
+
  ▀ █▄░█ ▄▀▀ ▀█▀ ▄▀▄ █▀▄ ▄▀▄
  █ █░▀█ ░▀▄ ░█░ █▀█ █░█ █░█
  ▀ ▀░░▀ ▀▀░ ░▀░ ▀░▀ ▀▀░ ░▀░
          .:created by sci_anto:.
-       """)
+       )
 post_url = input("Enter post url:") 
 download_url = "savefrom.net/"+post_url
-webbrowser.open(download_url)
+webbrowser.open(download_url
