f531864a3e16574280974c5dbb64832225346697
sci anto <65482441+Scianto@users.noreply.github.com>
2020-07-06 22:44:36 +0530

    Add files via upload

---
+++ Instado.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Instado v 1.1
+#this tool is mainly for Instagram post downloader
+#author : sci_anto
+#github : https://github.com/Scianto/Instado 
+
+printf "\e[34m ▀ █▄░█ ▄▀▀ ▀█▀ ▄▀▄ █▀▄ ▄▀▄  \e[0m\n"
+printf "\e[34m █ █░▀█ ░▀▄ ░█░ █▀█ █░█ █░█  \e[0m\n"
+printf "\e[34m ▀ ▀░░▀ ▀▀░ ░▀░ ▀░▀ ▀▀░ ░▀░  \e[0m\n"
+printf ""
+printf "\e[34m Created by Sci_anto \e[0m\n" 
+printf ""
+read -p $'\e[1;32m[\e[0m\e[1;77m?\e[0m\e[1;92m] Enter the post url:\e[0m ' url
+printf "\e[1;32m Your Post link:\e[0m https://savefrom.net/%s\n" $url 
